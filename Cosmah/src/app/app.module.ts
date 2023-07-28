744b953a4f63f0fa1d189e3f9c7546d6dd3b63cd
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-07-28 12:42:05 +0300

    have learnt routing

--- Cosmah/src/app/app.module.ts
+++ Cosmah/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';//importing routes
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    CharactersListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //building routes references
+    RouterModule.forRoot([
+      {
+        path: 'movies', //path
+        component:MoviesListComponent //component class name
+      },
+      {
+        path: 'characters',
+        component: CharactersListComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
